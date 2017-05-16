a5be55aaf7f08bda9249dd5b64e80fb279554c52
simenffredriksen <sffred16@student.uia.no>
2017-05-16 10:49:19 +0200

    3b

--- Oppg3/scanner.go
+++ Oppg3/scanner.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
 func main() {
-	scanLines()
-	scanRunes()
-	scanWords()
+	fileInfo := os.Args[1]
+
+	scanLines(fileInfo, "\n")
+	scanRunes(fileInfo, "\n")
+	scanWords(fileInfo, "\n")
+	temp := check(fileInfo)
+	freqRunes(temp)
+
 }
-func scanLines() {
-	file, err := os.Open("text1.txt")
+func scanLines(fileInfo string, search string) int {
+	file, err := os.Open(fileInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,11 +36,12 @@ func scanLines() {
 		fmt.Fprintln(os.Stderr, "reading input:", err)
 	}
 	fmt.Printf("Lines: %d\n", count)
+	return count
 }
 
-func scanRunes() {
+func scanRunes(fileInfo string, search string) int {
 	// Open file and create scanner on top of it
-	file, err := os.Open("text1.txt")
+	file, err := os.Open(fileInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,12 +60,13 @@ func scanRunes() {
 		fmt.Fprintln(os.Stderr, "reading input:", err)
 	}
 	fmt.Printf("Runes: %d\n", count)
+	return count
 
 }
 
-func scanWords() {
+func scanWords(fileInfo string, search string) int {
 	// An artificial input source.
-	file, err := os.Open("text1.txt")
+	file, err := os.Open(fileInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -73,5 +82,59 @@ func scanWords() {
 		fmt.Fprintln(os.Stderr, "reading input:", err)
 	}
 	fmt.Printf("Words: %d\n", count)
+	return count
 
+}
+
+func checkRune(filename string, search string) int {
+	//Åpner filen
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buffer := make([]byte, 512*1024)
+	//Teller linjeskift
+	counter := 0
+	//Søker på linjeskift
+	searchFor := []byte(search)
+
+	c, _ := file.Read(buffer)
+	counter += bytes.Count(buffer[:c], searchFor)
+	return counter
+}
+
+func check(filename string) map[int]string {
+	m := make(map[int]string)
+
+	// Runer for Ascii code valgt:
+	for i := 0x20; i <= 0x7F; i++ {
+		count := checkRune(filename, string(i))
+		rune := string(i)
+		m[count] = rune
+	}
+	fmt.Println()
+	return m
+}
+
+func freqRunes(m map[int]string) {
+	//preallocate the map size
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	// Print 5 største:
+	fmt.Println("5 mest brukte runer:")
+	value1 := keys[len(keys)-1]
+	value2 := keys[len(keys)-2]
+	value3 := keys[len(keys)-3]
+	value4 := keys[len(keys)-4]
+	value5 := keys[len(keys)-5]
+	fmt.Println("Antall:", value1, "Rune:", m[value1])
+	fmt.Println("Antall:", value2, "Rune:", m[value2])
+	fmt.Println("Antall:", value3, "Rune:", m[value3])
+	fmt.Println("Antall:", value4, "Rune:", m[value4])
+	fmt.Println("Antall:", value5, "Rune:", m[value5])
 }
